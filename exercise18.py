115fcafd6e4613cef67e63881b599693fe2edc93
alviz <azhigulskiy@gmail.com>
2020-04-09 22:56:45 +0300

    test

---
+++ exercise18.py
@@ -0,0 +1,12 @@
+from random import randint,choice,sample
+
+def game(original, user_number):
+
+    return number
+
+if __name__=="__main__":
+    print("Welcome to the Cows and Bulls Game!")
+    number = sample(range(1, 10), 4)
+    user_number = 1111
+    # user_num = input("Enter a number: ")
+    print(game(number, user_number))
