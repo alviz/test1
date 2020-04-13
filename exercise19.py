7cae44ea6ef880a363d19261884347dfa534f869
alviz <azhigulskiy@gmail.com>
2020-04-13 22:29:49 +0300

    ex19 add

--- exercise19.py
+++ exercise19.py
@@ -1,13 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-url = 'https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture'
+url = 'https://www.cnet.com/reviews/thinkpad-x1-carbon-7th-gen-review/'
 r = requests.get(url)
+file = open('web/log.log','w')
+
+# print(timeit.timeit(BeautifulSoup(r.text, "lxml")))
+# start_time = time.time()
 
 soup = BeautifulSoup(r.text, "lxml")
+# soup = BeautifulSoup(r.text, "html.parser")
 
-#print(soup)
-titles = soup.find_all("p")
+# end_time = time.time()
+# print(end_time-start_time)
+# print(soup)
+containers = soup.find_all("article")
 
-for story_heading in titles:
-    print(story_heading.text)
+for element in containers:
+    print(element.text)
+    if hasattr(element,'href'):
+        print(element['href'])
+        file.write(element.text + '\n')
+    else:
+        print('NONE')
+file.close()
+
+#0.032446861267089844
+#0.030868053436279297
---
+++ exercise20.py
@@ -0,0 +1,26 @@
+from random import randint,choice,sample
+import time
+
+def game(original, user_number):
+
+    return number
+
+if __name__=="__main__":
+    print("Welcome to the binari search!")
+    # number = sorted(sample(range(1, 1000), 100))
+    number = [10, 28, 38, 39, 47, 55, 74, 84, 97, 98, 105, 107, 132, 135, 143, 146, 152, 159, 167, 171, 172, 175, 184, 206, 212, 223, 225, 249, 251, 261, 276, 283, 286, 297, 331, 332, 336, 338, 346, 360, 388, 390, 407, 418, 434, 435, 436, 447, 474, 483, 522, 538, 543, 554, 564, 583, 590, 602, 618, 625, 642, 654, 665, 676, 678, 714, 718, 745, 747, 760, 777, 780, 781, 788, 795, 798, 821, 825, 830, 839, 841, 853, 858, 865, 869, 870, 877, 892, 894, 895, 897, 930, 963, 965, 973, 979, 983, 985, 987, 992]
+    print(number)
+
+    print(time.time())
+    start_time = time.time()
+
+    if 100 in number:
+        print('yes')
+    else:
+        print('no')
+
+    end_time = time.time()
+    print(end_time-start_time)
+
+    # user_num = input("Enter a number: ")
+    # print(game(number, user_number))
